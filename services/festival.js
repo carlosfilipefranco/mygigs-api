8d96894859ddc38d7566e6df93422dc670e9a040
Carlos Franco <cfranco@grupopie.com>
2023-06-22 10:28:55 +0100

    Update festival

--- services/festival.js
+++ services/festival.js
@@ -8,11 +8,11 @@ async function getMultiple(page = 1, search = null) {
 	if (search) {
 		searchQuery = `WHERE LOWER(festival.name) LIKE '%${search}%'`;
 	}
-	const rows = await db.query(`SELECT festival.id, festival.name FROM festival ${searchQuery} ORDER by name LIMIT ${offset},${config.listPerPage}`);
+	const rows = await db.query(`SELECT festival.id, festival.name, festival.image FROM festival ${searchQuery} ORDER by name LIMIT ${offset},${config.listPerPage}`);
 
 	// createEditions(rows);
 
-	populateEditionEvent(rows);
+	// populateEditionEvent();
 
 	let count = rows.length;
 	if (!search) {
@@ -30,8 +30,13 @@ async function getMultiple(page = 1, search = null) {
 }
 
 async function get(id) {
-	const result = await db.query(`SELECT festival.id, festival.name, edition.id AS edition_id, edition.name AS edition_name FROM festival JOIN edition ON festival.id = edition.festival_id WHERE festival.id = ${id}`);
-	return result;
+	const festival = await db.query(`SELECT festival.id, festival.name, festival.image FROM festival WHERE festival.id = ${id}`);
+	const editions = await db.query(`SELECT festival.id, festival.name, edition.id AS edition_id, edition.name AS edition_name FROM festival JOIN edition ON festival.id = edition.festival_id WHERE festival.id = ${id} ORDER by date_start`);
+
+	return {
+		festival: festival[0],
+		editions
+	};
 }
 
 async function getEdition(editionId) {
@@ -63,39 +68,6 @@ async function getEdition(editionId) {
 	} catch (error) {
 		console.error("Ocorreu um erro ao obter os gigs por edition:", error);
 		throw error;
-	}
-}
-
-async function createEditions(rows) {
-	for (const festival of rows) {
-		const venues = await db.query(`SELECT id FROM venue WHERE name = '${festival.name}'`);
-		for (const venue of venues) {
-			const gigs = await db.query(`SELECT gig.id, gig.date, gig.venue_id, gig.city_id FROM gig WHERE venue_id=${venue.id} ORDER BY gig.date`);
-
-			let currentYear = null;
-			let date_start = null;
-			let date_end = null;
-
-			for (const gig of gigs) {
-				const gigYear = new Date(gig.date).getFullYear();
-
-				if (currentYear !== gigYear) {
-					if (currentYear) {
-						await db.query(`INSERT INTO edition (festival_id, name, venue_id, city_id, date_start, date_end) VALUES (${festival.id}, '${festival.name} ${currentYear}', ${venue.id}, ${gig.city_id}, '${date_start}', '${date_end}')`);
-					}
-
-					currentYear = gigYear;
-					date_start = new Date(gig.date).toISOString().slice(0, 19).replace("T", " ");
-					date_end = new Date(gig.date).toISOString().slice(0, 19).replace("T", " ");
-				} else {
-					date_end = new Date(gig.date).toISOString().slice(0, 19).replace("T", " ");
-				}
-			}
-
-			if (currentYear) {
-				await db.query(`INSERT INTO edition (festival_id, name, venue_id, city_id, date_start, date_end) VALUES (${festival.id}, '${festival.name} ${currentYear}', ${venue.id}, ${gigs[0].city_id}, '${date_start}', '${date_end}')`);
-			}
-		}
 	}
 }
 
