9266b424aa41d993b8222962a9107caea8a359d5
Carlos Franco <cfranco@grupopie.com>
2024-11-13 18:05:12 +0000

    Change db to railway

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 const config = {
 	db: {
-		host: "127.0.0.1",
+		host: "mysql.railway.internal",
 		user: "root",
-		password: "root",
-		database: "mygigs"
+		password: "JTjmNVhzqmjFjdrUPxJqeYRivsDAfVAZ",
+		database: "railway"
 	},
 	// db: {
-	// 	host: "sql.freedb.tech",
-	// 	user: "freedb_mygigs",
-	// 	password: "PHf2rntk2M3!Cq9",
-	// 	database: "freedb_mygigs"
+	// 	host: "127.0.0.1",
+	// 	user: "root",
+	// 	password: "root",
+	// 	database: "mygigs"
 	// },
-
 	listPerPage: 20
 };
 
