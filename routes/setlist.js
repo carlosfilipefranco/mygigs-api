3c1b56581f7922f7e3c96b64f00b88e6d5523c85
Carlos Franco <cfranco@grupopie.com>
2025-06-27 11:17:02 +0100

    Add gig id to route

--- routes/setlist.js
+++ routes/setlist.js
@@ -5,8 +5,8 @@ const setlistService = require("../services/setlist");
 /* GET setlists */
 router.get("/", async function (req, res, next) {
 	try {
-		const { artist, city, date } = req.query;
-		const data = await setlistService.getSetlist(artist, city, date);
+		const { gigId, artist, city, date } = req.query;
+		const data = await setlistService.getSetlist(gigId, artist, city, date);
 		res.json(data);
 	} catch (err) {
 		console.error("Erro ao obter setlist.fm:", err.message);
