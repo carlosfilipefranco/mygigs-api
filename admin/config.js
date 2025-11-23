626e99d0476ddbefe5203b2b0ad0cca1f7fdaa63
Carlos Franco <cfranco@grupopie.com>
2025-11-23 18:52:36 +0000

    Fix port

--- admin/config.js
+++ admin/config.js
@@ -1,3 +1,5 @@
+const port = process.env.PORT || 3001;
+
 module.exports = {
-	API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3001/api"
+	API_BASE_URL: process.env.API_BASE_URL || `http://localhost:${port}/api`
 };
